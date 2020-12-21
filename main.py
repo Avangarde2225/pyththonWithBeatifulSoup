637454d7de26b3ddb34bb896a26087fc6ecb4c02
Serdar <serdar2225@gmail.com>
2020-12-20 23:30:29 -0500

    priting the first company using replace and text methods

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text,'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-print(jobs)
+#jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = soup.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+print(company_name)
