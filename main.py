d2eb482ba1e6f7528e0667bb3cc6c4b3659bb9ee
Serdar <serdar2225@gmail.com>
2020-12-21 23:49:00 -0500

    rearranging the method

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ soup = BeautifulSoup(html_text,'lxml')
 jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
 for job in jobs:
-#jobs = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-    company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-    skills = job.find('span', class_='srp-skills').text.replace(' ', '')
     publish_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in publish_date:
+#jobs = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
 
-    print(f'''
-    Company Name : {company_name}
-    Required Skills : {skills}
-       ''')
+
+        print(f'''
+        Company Name : {company_name}
+        Required Skills : {skills}
+         ''')
+
+
+          
 
 
 
