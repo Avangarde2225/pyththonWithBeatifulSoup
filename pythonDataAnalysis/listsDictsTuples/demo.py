543c98e303193548516ced889d4ff36ad75dd661
Serdar <serdar2225@gmail.com>
2021-01-31 22:43:40 -0500

    lists, tuples, sets, dictionaries

---
+++ pythonDataAnalysis/listsDictsTuples/demo.py
@@ -0,0 +1,24 @@
+node_1 = "custom object"
+
+my_data_type= [1,2,False,4, "mashur", None, node_1, 5.0,1]
+
+print(my_data_type)
+print(type(my_data_type))  #lists
+print(my_data_type[4])
+
+#dictionaries
+
+dict_one = {1:"hello", 2:"world"}
+print(dict_one)
+print(type(dict_one))
+print(dict_one[2])
+
+#sets
+my_setdata_type= {1,2,False,4, "mashur", None, node_1, 5.0,1}
+print(my_setdata_type)
+
+#tuples
+
+my_tuples = (1,2, False, 4, None)
+print(my_tuples)
+print(my_tuples[3])
