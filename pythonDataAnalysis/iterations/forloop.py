06c58eb682f1d61cecfd1a2fe75bbff7c4e23309
Serdar <serdar2225@gmail.com>
2021-02-14 23:37:53 -0500

    cont.ed iterations

--- pythonDataAnalysis/iterations/forloop.py
+++ pythonDataAnalysis/iterations/forloop.py
@@ -21,3 +21,21 @@ for item in my_dict:
 
 for key,value in my_dict.items():
     print(f"key is {key}, and value is {value}")
+
+print("*"* 40)
+
+#generate 100 random integers between 1 and 100
+from random import randint
+l1 = []
+for num in range(100):
+    l1.append(randint(1,100))
+print(l1)
+
+#another way
+
+l1 = [randint(1,100) for num in range(100)]
+print(l1)
+
+l2 = [num for num in range(100)]   #orderly way
+print(l2)
+
