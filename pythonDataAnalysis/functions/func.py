98ecfc2bd649d776c34304a6359298079e4553e4
Serdar <serdar2225@gmail.com>
2021-02-16 00:54:32 -0500

    small improvements

--- pythonDataAnalysis/functions/func.py
+++ pythonDataAnalysis/functions/func.py
@@ -11,4 +11,4 @@ food_result = get_input_from_user()
 print("What did you show ending did you dislike the mosdt")
 show_result = get_input_from_user()
 
-print(f"To summarize : Your name is {name_result}, you ate {food_result} food today and hated the ending of {show_result}")
+print(f"To summarize : Your name is {name_result.capitalize()}, you ate {food_result} food today and hated the ending of {show_result.upper()}")
